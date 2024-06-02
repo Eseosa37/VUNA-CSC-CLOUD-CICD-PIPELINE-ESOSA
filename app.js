e6a85697e2e7b17c47f5f740363f5c05839c56ad
ESEOSA <eseosamartin57@gmail.com>
2024-06-02 18:06:25 -0500

    update

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(express.static("public"))
+app.get('/', (req, res) => res.send(`THIS IS CSC 314 CLOUD COMPUTING DEVOPS; CI-CD PIPELINE CREATED
+    WITH AWS. THIS PAGE INDICATES SUCCESS. 
+    YOURNAME ----------------
+    MATRIC NUMBER ------------`
+    ));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
